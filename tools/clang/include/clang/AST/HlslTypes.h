3f3a86dfbf8452d7994047036eddfbde0f2e32de
anupamac <anupamac@nvidia.com>
2025-02-26 18:32:21 -0800

    Add Warning/Error for missing attribute SM69 plus

--- tools/clang/include/clang/AST/HlslTypes.h
+++ tools/clang/include/clang/AST/HlslTypes.h
@@ -475,6 +475,7 @@ bool IsHLSLObjectWithImplicitROMemberAccess(clang::QualType type);
 bool IsHLSLRWNodeInputRecordType(clang::QualType type);
 bool IsHLSLRONodeInputRecordType(clang::QualType type);
 bool IsHLSLNodeOutputType(clang::QualType type);
+bool IsHLSLNodeOutputArrayType(clang::QualType type);
 
 DXIL::NodeIOKind GetNodeIOType(clang::QualType type);
 
